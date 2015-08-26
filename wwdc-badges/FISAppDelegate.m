dd15d4dba6e1337c48b7eb6805037a6b8a1f5e7f
sean-wantwave <sean@wantwave.com>
2015-08-26 16:53:32 -0700

    Completed array lab

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -12,11 +12,19 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        NSLog(@"Hello, my name is %@", conferenceSpeakers[i]);
+    }
+    
+    NSArray *conferenceSpeakersFirstName = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    
+    NSArray *conferenceSpeakersLastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakersFirstName count]; i++) {
+        NSLog(@"Hello, my name is %@ %@", conferenceSpeakersFirstName[i], conferenceSpeakersLastName[i]);
+    }
     
     return YES;
 }
