45bb41b6c7dfd76fd3e6715118643295a6464efc
jspringer <jsnsprngr@gmail.com>
2015-10-04 14:29:35 -0400

    Seventh

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,12 @@
      
      */
     
+    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage" ];
+    
+    for (NSUInteger i=0; i < 8; i++) {
+        NSLog(@"Hello, my name is %@!", conferenceSpeakers[i] );
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
