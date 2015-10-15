d6c65a20ce04c9c6992cd6430cebafa9fac74fb0
troydo42 <hoangdov@gmail.com>
2015-10-15 11:32:48 -0700

    Playing with Array in Obj-C

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -6,15 +6,28 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    //Create an NSArray object named conferenceSpeakers to sotre the list of speaker names as strings
     
-    // do not alter
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    
+    //Iterate over the conferenceSpeakers array using a for loop.
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        NSLog(@"Hello, my name is %@", conferenceSpeakers[i]);
+    }
+    
+    
+    //Spit name into 2 array: first name and last name
+    
+    NSArray *speakersFirstName = @[@"Aniata", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *speakersLastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger m= 0; m < [speakersFirstName count] ; m++) {
+        NSLog(@"Hello, my name is %@ %@", speakersFirstName[m], speakersLastName[m]);
+    }
+    
     return YES;  //
     ///////////////
 }
