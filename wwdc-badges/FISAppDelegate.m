f7b425f8108feb1ebae862102452ef01c0a9e281
eoinwhitney <whitneyeoin@gmail.com>
2015-11-11 14:02:25 -0500

    wwdc-badges-001-prework-ios

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    NSArray *firstName = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *lastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+  //  NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for (NSUInteger i=0; i<=7; i++) {
+        NSLog(@"Hello, my name is %@ %@.", firstName[i], lastName[i]);
+        
+    }
     
     // do not alter
     return YES;  //
