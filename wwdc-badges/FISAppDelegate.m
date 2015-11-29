90595b448c421736745b3f3e8195cbb03bce5e4a
lianaluci <llgrobstein@gmail.com>
2015-11-29 00:30:22 -0500

    comment out first section, print advanced section to console

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,16 +8,21 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage" ];
+//  NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage" ];
     
-    for ( NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+//   for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
     
-        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
-    }
+//       NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+//   }
     
+    NSArray *conferenceSpeakersFirstName = @[ @"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles" ];
  
+    NSArray *conferenceSpeakersLastName = @[ @"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage" ];
     
-   
+    for (NSUInteger i = 0; i < [conferenceSpeakersFirstName count]; i++) {
+        
+        NSLog(@"Hello, my name is %@ %@.", conferenceSpeakersFirstName[i], conferenceSpeakersLastName[i]);
+    }
     
     // do not alter
     return YES;  //
