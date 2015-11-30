8881bb391145c2f87b434a94059f3880f85a1e80
Gn3iss <j.s.roberts41@gmail.com>
2015-11-30 07:21:34 -0500

    wwdc badges advanced

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -22,6 +22,15 @@
     NSLog(@ "Hello, my name is Grace Hopper.");
     NSLog(@ "Hello, my name is Charles Babbage.");
     
+    NSLog(@"%@ %@", @"Hello, my name is Anita", @"Borg.");
+    NSLog(@"%@ %@", @"Hello, my name is Alan", @"Kay.");
+    NSLog(@"%@ %@", @"Hello, my name is Ada", @"Lovelace.");
+    NSLog(@"%@ %@", @"Hello, my name is Aaron", @"Swartz.");
+    NSLog(@"%@ %@", @"Hello, my name is Alan", @"Turing.");
+    NSLog(@"%@ %@", @"Hello, my name is Michael", @"Faraday.");
+    NSLog(@"%@ %@", @"Hello, my name is Grace", @"Hopper.");
+    NSLog(@"%@ %@", @"Hello, my name is Charles", @"Babbage.");
+    
     
     
     // do not alter
