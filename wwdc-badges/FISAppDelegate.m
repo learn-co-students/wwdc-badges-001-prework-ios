d06d9044401dc0cfabeb80bb94d7ef7bf7cdfeeb
Angie-Huang <angelay.huang@yahoo.com>
2015-12-07 23:44:07 -0500

    Complete Badges program along with advance portion

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,29 @@
      
      */
     
+   /* 
+    
+    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage" ];
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i] );
+    }
+    
+    */
+    
+    //Advance
+    
+    NSArray *firstName = @[ @"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles" ];
+    NSArray *lastName = @[ @"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage" ];
+   
+    for (NSUInteger i = 0; i < [firstName count]; i++) {
+        
+        NSLog(@"Hello, my name is %@ %@.", firstName[i], lastName[i] );
+    }
+
+    
+    
     // do not alter
     return YES;  //
     ///////////////
