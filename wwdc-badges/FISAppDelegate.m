e6e7e0e3f41ccc78e8826e2d987a837b3e2c7362
FedericoPaliotta <federico.paliotta@gmail.com>
2015-12-10 07:06:06 +0100

    Done.

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,45 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+   
+    
+//    NSArray *conferenceSpeakers = @[@"Anita Borg",
+//                                    @"Alan Kay",
+//                                    @"Ada Lovelace",
+//                                    @"Aaron Swartz",
+//                                    @"Alan Turing",
+//                                    @"Michael Faraday",
+//                                    @"Grace Hopper",
+//                                    @"Charles Babbage"];
+//    
+//    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+//        NSLog(@"Hello, my name is %@", conferenceSpeakers[i]);
+//    }
+    
+    NSArray *conferenceSpeakersFirstNames = @[@"Anita",
+                                              @"Alan",
+                                              @"Ada",
+                                              @"Aaron",
+                                              @"Alan",
+                                              @"Michael",
+                                              @"Grace",
+                                              @"Charles"];
+    
+    NSArray * conferenceSpeakersLastNames = @[@"Borg",
+                                              @"Kay",
+                                              @"Lovelace",
+                                              @"Swartz",
+                                              @"Turing",
+                                              @"Faraday",
+                                              @"Hopper",
+                                              @"Babbage"];
+    // Just to(o) be safer
+    NSUInteger limit = [conferenceSpeakersFirstNames count] <= [conferenceSpeakersLastNames count] ?[conferenceSpeakersFirstNames count] : [conferenceSpeakersLastNames count];
+    
+    for (NSUInteger i = 0; i < limit; i++) {
+        NSLog(@"Hello, my name is %@ %@", conferenceSpeakersFirstNames[i], conferenceSpeakersLastNames[i]);
+    }
+    
     
     // do not alter
     return YES;  //
