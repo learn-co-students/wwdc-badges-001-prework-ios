e151c06bf5920871bccd1ee5333df733397f2610
abcheromar <abcheromar@gmail.com>
2015-12-20 03:44:30 -0500

    my badges lab last version after issues with with Learn app

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,7 +8,37 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
 
-/whats this? 
+ //  FISAppDelegate.m
+
+#import "FISAppDelegate.h"
+
+@implementation FISAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+   
+    
+   
+    
+    NSArray  *firstName = @[ @"Anita", @"Alan", @"Ada", @"Aaron",
+                            @"Alan", @"Michael", @"Grace", @"Charles"];
+    
+    NSArray  *lastName = @[ @"Borg", @"Kay", @"Lovelace", @"Swartz",
+                         @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    
+    for (NSUInteger i = 0; i < [firstName count]; i++) {
+        NSLog(@"Hello, my name is %@ %@.", firstName[i], lastName[i] );
+    }
+    
+    
+    
+    // do not alter
+    return YES;  //®
+    ///////////////
+}
+
+@end
 
 
 
