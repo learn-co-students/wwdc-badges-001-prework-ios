5441df6011095ae37a842e00e13886e7dd2291fd
ArielSD <ascottdicker@dosomething.org>
2016-01-05 09:41:47 -0500

    Done

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -12,13 +12,43 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    /*NSArray *speakers = @[@"Adele Goldberg",
+                          @"Edsger Dijkstra",
+                          @"Joan Clarke",
+                          @"Clarence Ellis",
+                          @"Margaret Hamilton",
+                          @"George Boole",
+                          @"Tim Berners-Lee",
+                          @"Jean Bartik"];
+     */
+    
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    NSMutableString *badge = [[NSMutableString alloc] init];
+    [badge appendFormat:@"Hello, my name is %@.", speaker];
+    return badge;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i  < [speakers count]; i++) {
+        NSString *helloMyNameIs = [self badgeForSpeaker:speakers[i]];
+        [badges addObject:helloMyNameIs];
+    }
+    return badges;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *greetingAndRoomList = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSUInteger dressingRoom = i + 1;
+        NSMutableString *welcome = [@"Welcome, " mutableCopy];
+        [welcome appendFormat:@"%@! You'll be in dressing room %lu.", speakers[i], dressingRoom];
+        [greetingAndRoomList addObject:welcome];
+    }
+    return greetingAndRoomList;
+}
 
 @end
