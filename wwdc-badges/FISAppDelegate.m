5d03b3c4e511869ad396e67f78835e2dedf623db
bclous <brian.clouser@gmail.com>
2016-01-10 19:58:58 -0500

    finished the lab

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,27 @@
      
      */
     
+    
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for (NSUInteger i=0; i < [conferenceSpeakers count]; i++)
+    {
+        
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+        
+    }
+    
+    NSArray *firstNames = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *lastNames = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger j = 0; j < [firstNames count]; j++)
+    {
+        NSLog(@"Hello, my name is %@ %@.",firstNames[j], lastNames[j]);
+    }
+    
+    
+    
+    
     // do not alter
     return YES;  //
     ///////////////
