867d6c04c1ff94f5e21d787998175858db0a48c2
Elli Scharlin <Elli@Ellis-MacBook-Air.local>
2016-01-13 10:27:57 -0500

    advanced option also

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,19 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turig", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for (NSUInteger i = 0; i<[conferenceSpeakers count]; i++) {
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+    }
+    
+    NSArray *firstNames = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    
+    NSArray *lastNames = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turig", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i<[firstNames count]; i++) {
+        NSLog(@"Hello, my name is %@ %@.", firstNames[i], lastNames[i]);
+    }
     
     // do not alter
     return YES;  //
