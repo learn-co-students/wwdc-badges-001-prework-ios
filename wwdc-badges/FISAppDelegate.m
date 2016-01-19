7497d16cf4a4c3baaa4e5c4e28f4fe7f60ba7a04
volldammd <volldammd@gmail.com>
2016-01-19 15:56:07 -0500

    done

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -17,8 +17,19 @@
     NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
     
     for (NSString *item in conferenceSpeakers) {
-        NSLog(@"Helloi, my name is %@", item);
+        NSLog(@"Hello, my name is %@", item);
     }
+    
+    // expirmental below
+    
+    NSArray *lastnames = @[@"rg", @"Alay", @"Aace", @"Aawartz", @"Alring", @"Miaraday", @"Graoer", @"Chalebage"];
+    
+    for (NSString *item in lastnames) {
+        NSLog(@"Hello, my name is %@", item);
+    }
+        
+    
+    // experimental above
     
     // do not alter
     return YES;  //
