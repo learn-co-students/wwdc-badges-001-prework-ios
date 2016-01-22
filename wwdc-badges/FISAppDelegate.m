87ae9ba03446bf05421198e82fc00dcc549bf32c
davidvypark <davidvypark@gmail.com>
2016-01-21 19:32:14 -0500

    work with arrays w/ advanced section

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,30 @@
      
      */
     
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for (NSInteger i = 0; i <=7; i++){
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+    }
+    
+    
+    //Advanced
+    
+    NSArray *firstName = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *lastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Touring", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSInteger j = 0; j <=7; j++){
+        NSLog(@"Hello, my name is %@ %@.", firstName[j], lastName[j]);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // do not alter
     return YES;  //
     ///////////////
