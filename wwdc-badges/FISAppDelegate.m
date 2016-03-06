692b615f2afa576a870885690b270b4671c78f60
ganderson09 <anderson.gabriel09@gmail.com>
2016-03-06 01:22:21 -0500

    WWDC-Badges Complete

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,13 +8,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    NSArray *firstName = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan",                 @"Michael", @"Grace", @"Charles"];
     
-    // do not alter
+    NSArray *lastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i < [firstName count] && [lastName count]; i++) {
+        
+        NSLog(@"Hello, my name is %@ %@.", firstName[i], lastName[i]);
+
+    }
+
     return YES;  //
     ///////////////
 }
