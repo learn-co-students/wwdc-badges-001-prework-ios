e7bdf818746112004c6137ebf75447d6d9c92cf5
user116796 <gyk42@hotmail.com>
2016-03-20 13:54:11 -0400

    WWDC badge excercise

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -9,12 +9,24 @@
     // Override point for customization after application launch.
     
     /**
+     NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
      
-     * Write your solution here.
+     for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+     NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+     };
      
      */
     
     // do not alter
+    
+    
+    NSArray *speakersFirstName = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *speakersLastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i < 8; i++) {
+        NSLog(@"Hello, my name is %@ %@.", speakersFirstName[i], speakersLastName[i]);
+    }
+    
     return YES;  //
     ///////////////
 }
