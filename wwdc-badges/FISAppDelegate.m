99e50887130b723922580ef78bfc1b288721c7e7
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-03-27 16:28:11 -0400

    wwdc Badges did this one on my own

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -7,16 +7,20 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+ 
+    NSArray *conferenceFirst = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    NSArray *conferenceLast = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i < 8; i++) {
+            NSLog(@"Hello, my name is %@ %@.", conferenceFirst[i], conferenceLast[i]);
+        
+    }
+
     
     // do not alter
     return YES;  //
     ///////////////
-}
 
-@end
+}
+    @end
