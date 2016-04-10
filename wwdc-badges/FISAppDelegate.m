5925794ca359a2604c3bc030bf27c9ce7d07de46
dochonglo <ismaelbarry777@hotmail.com>
2016-04-10 16:09:22 -0400

    Lab 7: WWDC Badges is complete.

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,22 @@
      
      */
     
+    // Objective:
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelance", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    NSUInteger start = 0;
+    NSUInteger end = [conferenceSpeakers count];
+    
+    for (NSUInteger i =start; i<end; i++) {
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+    }
+    
+    // Advanced:
+    NSArray *conferenceSpeakersFirstName = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *conferenceSpeakersLastName = @[@"Borg", @"Kay", @"Lovelance", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    for (NSUInteger i = start ; i<end; i++) {
+        NSLog(@"Hello, my name is %@ %@.", conferenceSpeakersFirstName[i], conferenceSpeakersLastName[i]);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
