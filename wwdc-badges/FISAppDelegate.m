ef4c3a884ab8099c40a3984dba9fe1239691c253
mariogabriele1 <mgabriele89@gmail.com>
2016-04-18 17:19:45 -0400

    WWDC Badges

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,29 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    //Standard iteration
+   
+    
+    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Schwartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage" ];
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        NSLog(@"Hello my name is %@!", conferenceSpeakers[i] );
+    }
+    
+    //First name, Last name iteration
+
+    
+    NSArray *firstNames = @[ @"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles" ];
+    NSArray *lastNames = @[ @"Borg", @"Kay", @"Lovelace", @"Schwartz", @"Turing", @"Faraday", @"Hopper", @"Babbage" ];
+    
+    for (NSUInteger i = 0; i < [firstNames, lastNames count]; i++) {
+        NSLog(@"Hello my name is %@!", conferenceSpeakers[i] );
+    
+    
+    
+    
+    
+    
     
     // do not alter
     return YES;  //
