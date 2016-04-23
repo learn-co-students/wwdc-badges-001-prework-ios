5433ab456176fb736b40eccc2c4a92542eb4eedd
dochonglo <ismaelbarry777@hotmail.com>
2016-04-23 17:12:29 -0400

    Added lab objectives for clarification purposes.

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -4,6 +4,15 @@
 
 @implementation FISAppDelegate
 
+// Objectives:
+// 1. Create an array containing string objects.
+// 2. Iterate over an array with a for loop.
+// 3. Access an array's elements with subscripting syntax.
+// 4. Use an array's elements to print a dynamically interpolated string to the console.
+
+// Advanced:
+
+// 1. Instead of using a single array for names, split the names into two separate arrays for first name and last name. Alter your NSLog() to take two format arguments, one from each array: Hello, my name is <#firstName#> <#lastName#>.. You should get an equivalent result to the one shown above.
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
