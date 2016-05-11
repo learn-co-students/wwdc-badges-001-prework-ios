0c9450dc7f1ddcba6af61f76d77ca4946c4add70
Erica <erica@Ericas-Mac-mini.local>
2016-05-11 19:17:49 -0400

    many solutions

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,9 +14,52 @@
      
      */
     
+    
+    NSArray *conferenceSpeakers = @[
+                                    
+                                    @"Anita Borg",
+                                    @"Alan Kay",
+                                    @"Ada Lovelace",
+                                    @"Aaron Swartz",
+                                    @"Alan Turing",
+                                    @"Michael Faraday",
+                                    @"Grace Hopper",
+                                    @"Charles Babbage",
+                                    
+                                    ];
+    for (NSUInteger i=0; i< [conferenceSpeakers count]; i++) {
+        
+        
+        NSLog(@"Hello, my name is, %@.", conferenceSpeakers [i]);
+        
+        
+        
+        //this is the advanced solution:
+        
+        
+        
+        /*
+         NSArray *conferenceFirst = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+         
+         NSArray *conferenceLast = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+         
+         for (NSUInteger i = 0; i < 8; i++) {
+         NSLog(@"Hello, my name is %@ %@.", conferenceFirst[i], conferenceLast[i]);
+         
+         }
+*/
+       
+        // I believe there is more solutions than this...maybe using a splitting method....
+        
+        
+        
+    }
+
+    
     // do not alter
     return YES;  //
     ///////////////
 }
 
-@end
+    @end
+
