0746e48af6fd0941e38682ea7e3e58412a2fe274
Jordan Christley <lilj390150@live.com>
2016-06-14 08:27:21 -0500

    I have done the regular and advanced now :).

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -13,7 +13,18 @@
      * Write your solution here.
      
      */
-    
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    for(NSUInteger i = 0; i < [conferenceSpeakers count];i++) {
+        NSString *name = [conferenceSpeakers objectAtIndex:i];
+        NSLog(@"%@%@%@", @"Hello, my name is ", name, @".");
+    }
+    NSArray *confSpeakersLastNames = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    NSArray *confSpeakersFirstNames = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    for(NSUInteger i = 0; i < [confSpeakersFirstNames count]; i++) {
+        NSString *lastName = [confSpeakersLastNames objectAtIndex:i];
+        NSString *firstName = [confSpeakersFirstNames objectAtIndex:i];
+        NSLog(@"%@%@ %@%@", @"Hello, my name is ", firstName, lastName, @".");
+    }
     // do not alter
     return YES;  //
     ///////////////
