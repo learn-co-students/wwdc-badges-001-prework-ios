2d9717b07fb129c7324c86dc0e81bc6efbbf0e6c
William Robinson <wsrobins@uvm.edu>
2016-06-17 17:50:34 -0400

    Practice with arrays and string indexing // lab complete.

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,25 @@
      
      */
     
+    NSArray *conferenceSpeakers = 
+        @[@"Anita Borg", 
+        @"Alan Kay", 
+        @"Ada Lovelace", 
+        @"Aaron Swartz", 
+        @"Alan Turing", 
+        @"Michael Faraday", 
+        @"Grace Hopper", 
+        @"Charles Babbage"];
+
+    for (NSInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        
+        NSArray *splitNames = [conferenceSpeakers[i] componentsSeparatedByString:@" "];
+        NSString *firstName = splitNames[0];
+        NSString *lastName = splitNames[1];
+        
+        NSLog(@"Hello, my name is %@ %@.", firstName, lastName);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
