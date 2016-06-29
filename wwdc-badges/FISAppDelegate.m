58469275de25c880a60c12e00a21a801d236a902
chainsawbucktooth <chainsaw.bucktooth@gmail.com>
2016-06-29 07:30:25 -0700

    initial commit

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    NSArray *conferenceSpeakers = [NSArray arrayWithObjects:@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage", nil];
+    
+    NSUInteger speakerCount = [conferenceSpeakers count];
+    
+    for (NSUInteger i = 1; i<= speakerCount; i++) {
+        NSLog(@"Hellow, my name is  %@.", conferenceSpeakers[i]);
+    }
     
     // do not alter
     return YES;  //
