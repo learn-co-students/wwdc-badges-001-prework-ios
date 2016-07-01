6590f1d8cadf6b42c411e1a6d2fab725c7acbd2c
Felicity <felicity.johnson.mail@gmail.com>
2016-07-01 07:54:57 -0400

    badge added advanced

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -27,6 +27,20 @@
         
     }
     
+    
+    NSArray *conferenceSpeakersFirstName = @[@"Anita ", @"Alan ", @"Ada ", @"Aaron ", @"Alan ", @"Michael ", @"Grace ", @"Charles "];
+    NSArray *conferenceSpeakersLastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"uring", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 1; i < [conferenceSpeakersFirstName count]; i++) {
+        
+        NSString *hello = @"Hello, my name is ";
+
+        
+        NSLog(@"%@%@%@.", hello,conferenceSpeakersFirstName[i], conferenceSpeakersLastName[i]);
+        
+    }
+
+    
     return YES;  //
     ///////////////
 }
