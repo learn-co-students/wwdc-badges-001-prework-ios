15d272c4e3f1219210391d9077537e4b235001b4
Forrest Zhao <fozhao92@gmail.com>
2016-06-30 19:21:24 -0700

    completed

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,18 @@
      
      */
     
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for (NSString *name in conferenceSpeakers) {
+        NSLog(@"Hello, my name is %@.", name);
+    }
+    
+    NSArray *firstNames = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *lastNames = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i < [firstNames count]; i++) {
+        NSLog(@"Hello, my name is %@ %@.", firstNames[i], lastNames[i]);
+    }
     // do not alter
     return YES;  //
     ///////////////
