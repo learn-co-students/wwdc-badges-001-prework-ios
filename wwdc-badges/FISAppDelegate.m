331306105db9967f8a9ced1e8a2411b8792627ab
Henry Ly <henryly@gmail.com>
2016-07-31 18:34:40 -0700

    Done
    Included the advance step of separating the first and last names in to
    two separate arrays.

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,13 @@
      
      */
     
+    NSArray *conferenceSpeakersFirst = @[ @"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *conferenceSpeakersLast = @[ @"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakersFirst count]; i++) {
+        NSLog(@"Hello, my name is %@ %@.", conferenceSpeakersFirst[i], conferenceSpeakersLast[i]);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
