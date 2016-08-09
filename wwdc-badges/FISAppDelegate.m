9a7f620c731c0ce112179a70e0f0a466dfc8b76a
Paul Tangen <ptangen@ptangen.com>
2016-08-09 08:35:22 -0400

    complete the WWDC badges exercise

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    
+    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage" ];
+    //NSLog(@"%@", conferenceSpeakers);
+    
+    NSString *greeting = @"Hello, my name is";
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        NSLog(@"%@ %@.", greeting, conferenceSpeakers[i]);
+    }
+    
     
     // do not alter
     return YES;  //
