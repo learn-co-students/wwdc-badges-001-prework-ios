65555661cbed71c41f6bf0159104d1a1c29a750b
skakig <skakig@gmail.com>
2017-05-01 22:10:41 -0700

    My first array commit

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -13,7 +13,14 @@
      * Write your solution here.
      
      */
+    // WWDC Badges array
+    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Arron Swarts", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        NSLog(@"Hello, my name is %@!", conferenceSpeakers[i] );
+    }
     
+/* I'm tired and ready for bed. I will continue trying to find the solution for the advanced version tomorrow. Thanks for the great lessons!
+ */
     // do not alter
     return YES;  //
     ///////////////
